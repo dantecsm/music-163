cac3da232f7df702b9ecbd920929a75e6abb651a
dantecsm <fakersfather@qq.com>
2019-03-13 10:36:23 +0800

    添加歌曲主页和播放页

---
+++ src/js/index/page-1.js
@@ -0,0 +1,29 @@
+{
+  let view = {
+    el: '.page-1',
+    init() {
+    	this.$el = $(this.el)
+    },
+    show() {
+    	this.$el.addClass('active')
+    },
+    hide() {
+    	this.$el.removeClass('active')
+    }
+  }
+  let model = {}
+  let controller = {
+    init(view, model) {
+      this.view = view
+      this.model = model
+      this.view.init()
+      this.bindEventHub()
+    },
+    bindEventHub() {
+    	window.eventHub.on('selectTab', tabName => {
+    		tabName === 'page-1'? this.view.show(): this.view.hide()
+    	})
+    }
+  }
+  controller.init(view, model)
+}
